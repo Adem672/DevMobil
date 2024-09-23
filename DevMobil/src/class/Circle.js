65a4bdc07cabae15bb14917afaea29c147569961
Adem Ahmetovic <ademahmetovic2002@gmail.com>
2024-09-23 11:50:22 +0200

    create circles and move and speed random

---
+++ DevMobil/src/class/Circle.js
@@ -0,0 +1,33 @@
+export default class Circle {
+    constructor({
+        radius = 100,
+        color = "tomato",
+        x = 0,
+        y = 0,
+        speed = 100,
+        dir = 0,
+    } = {}) {
+        this.radius = radius;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.dir = dir;
+    }
+
+    move(dt) {
+        const dx = Math.cos(this.dir) * this.speed * dt / 1000;
+        const dy = Math.sin(this.dir) * this.speed * dt / 1000;
+        this.x += dx;
+        this.y += dy;
+    }
+
+    draw(ctx) {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.closePath()
+    }
+
+}
---
+++ DevMobil/src/main.js
@@ -0,0 +1,38 @@
+import Circle from './class/Circle.js';
+
+const ctx = document.querySelector('canvas').getContext('2d');
+
+// set canvas size to match the parent container
+ctx.canvas.width = ctx.canvas.clientWidth;
+ctx.canvas.height = ctx.canvas.clientHeight;
+
+const circles = [];
+for (let i = 0; i < 100; i++) {
+  circles.push(new Circle({
+    x: Math.random() * ctx.canvas.width,
+    y: Math.random() * ctx.canvas.height,
+    speed: Math.random() * 100,
+    dir: Math.random() * Math.PI * 2,
+    color: `hsl(${Math.random() * 360}, 75%, 50%)`,
+  }));
+}
+
+// raf loop
+function tick(timestamp) {
+  const dt = timestamp - lastTick;
+  lastTick = timestamp;
+
+  // Update the world
+  for (const circle of circles) circle.move(dt);
+
+  // Render the WORLD
+  ctx.canvas.width = ctx.canvas.clientWidth;
+  ctx.canvas.height = ctx.canvas.clientHeight;
+  for (const circle of circles) circle.draw(ctx);
+
+  // Main animation loop
+  requestAnimationFrame(tick);
+}
+
+let lastTick = 0;
+requestAnimationFrame(tick);
